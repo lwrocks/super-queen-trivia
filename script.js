484927d147ec3d69cd4f9b7bac712cb7d4976c07
Laurel Wright <wright.laureld@gmail.com>
2020-01-20 15:36:28 +0100

    Updated code for game logic and added stylesheet

--- script.js
+++ script.js
@@ -1,82 +1,114 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-
-function buildQuiz() {
-    const outPut = [
-
-    ];
-    // For each question
-    myQuestions.forEach(
+(function(){
+    function buildQuiz(){
+      // variable to store the HTML output
+      const output = [];
+  
+      // for each question...
+      myQuestions.forEach(
         (currentQuestion, questionNumber) => {
-            // Store list of answer choices
-            const answers = [
-
-            ]
-            // For each available answer 
-            for (letter in currentQuestion.answers) {
-                // Add an HTML radio button
-                answers.push(
-                    <label>
-                        <input type="radio" name="question${questionNumber}" value="${letter}">
-                            ${letter}:
-                            ${currentQuestion.answers[letter]}
-                        </input>
-                    </label>
-                );
-            }
-            // Add the questions ans its answers to the output
-            output.push(
-                <div class="question">${currentQuestion.question}</div>
-                <div class="answers">${answers.join('')}</div>
+  
+          // variable to store the list of possible answers
+          const answers = [];
+  
+          // and for each available answer...
+          for(letter in currentQuestion.answers){
+  
+            // ...add an HTML radio button
+            answers.push(
+              `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+              </label>`
             );
+          }
+  
+          // add this question and its answers to the output
+          output.push(
+            `<div class="question"> ${currentQuestion.question} </div>
+            <div class="answers"> ${answers.join('')} </div>`
+          );
         }
-    );
-    
-}
-
-function showResults() {
-
-}
-
-// Display quiz right away
-buildQuiz();
-
-// On submit, show results
-submitButton.addEventListener('click', showResults);
-
-// Quiz Questions 
-const quizQuestions = [
-    {
+      );
+  
+      // finally combine our output list into one string of HTML and put it on the page
+      quizContainer.innerHTML = output.join('');
+    }
+  
+    function showResults(){
+  
+      // gather answer containers from our quiz
+      const answerContainers = quizContainer.querySelectorAll('.answers');
+  
+      // keep track of user's answers
+      let numCorrect = 0;
+  
+      // for each question...
+      myQuestions.forEach( (currentQuestion, questionNumber) => {
+  
+        // find selected answer
+        const answerContainer = answerContainers[questionNumber];
+        const selector = `input[name=question${questionNumber}]:checked`;
+        const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+  
+        // if answer is correct
+        if(userAnswer === currentQuestion.correctAnswer){
+          // add to the number of correct answers
+          numCorrect++;
+  
+          // color the answers green
+          answerContainers[questionNumber].style.color = 'lightgreen';
+        }
+        // if answer is wrong or blank
+        else{
+          // color the answers red
+          answerContainers[questionNumber].style.color = 'red';
+        }
+      });
+  
+      // show number of correct answers out of total
+      resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+    }
+  
+    const quizContainer = document.getElementById('quiz');
+    const resultsContainer = document.getElementById('results');
+    const submitButton = document.getElementById('submit');
+    const myQuestions = [
+      {
         question: "When did the first season of RuPaul's Drag Race premiere?",
         answers: {
-            a: "2006",
-            b: "2007",
-            c: "2012",
-            d: "2009"
+          a: "2006",
+          b: "2007",
+          c: "2012",
+          d: "2009"
         },
         correctAnswer: "d"
-    },
-
-    {
-        question: "Who was the first winner of Drag Race All Stars?",
+      },
+      {
+        question: "Who was the first winner of Drag Race All-Stars?",
         answers: {
-            a: "Pandora Boxx",
-            b: "Chad Michaels",
-            c: "Manila Luzon",
-            d: "Raven",
+          a: "Pandora Boxx",
+          b: "Chad Michaels",
+          c: "Manila Luzon",
+          d: "Raven"
         },
         correctAnswer: "b"
-    },
-
-    {
+      },
+      {
         question: "When did Michelle Visage first become a judge on RuPaul's Drag Race?",
         answers: {
-            a: "Season 1",
-            b: "Season 5",
-            c: "Season 3",
-            d: "Season 6",
+          a: "Season 1",
+          b: "Season 5",
+          c: "Season 3",
+          d: "Season 6"
         },
         correctAnswer: "c"
-    },
-]
+      }
+    ];
+  
+    // Kick things off
+    buildQuiz();
+  
+    // Event listeners
+    submitButton.addEventListener('click', showResults);
+  })();
